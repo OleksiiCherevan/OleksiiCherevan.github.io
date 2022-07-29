372b3d666366a3fc3cd791b83d51bc01401de36c
Oleksii <lolmagexe@gmail.com>
2022-07-29 12:29:57 +0300

    (feature) updated header mobile/tablet

--- src/components/01-molecules/05-bars/NavHeaderMobile/index.jsx
+++ src/components/01-molecules/05-bars/NavHeaderMobile/index.jsx
@@ -1,8 +1,9 @@
 import style from "./index.module.css";
 import React, { useState } from "react";
 import LinkText from "components/00-atoms/01-links/LinkText";
-
+import { SOCIALS } from "assets/static";
 import { MENU_BURGER, MENU_CLOSE } from "assets/static";
+import NavSocials from "../NavSocials";
 const NavHeaderMobile = (props) => {
     const { children, navs = [] } = props;
 
@@ -32,18 +33,23 @@ const NavHeaderMobile = (props) => {
                                 <a
                                     className={style["nav-button"]}
                                     href={nav.href}
-                                >{nav.text}</a>
+                                >
+                                    {nav.text}
+                                </a>
                             );
                         })}
                     </nav>
                 </>
             ) : (
-                <button
-                    className={`${style["open"]} ${style["button"]} `}
-                    onClick={handleOpen}
-                >
-                    {MENU_BURGER}
-                </button>
+                <div className={`${style["block"]} ${style["block_mobile"]}`}>
+                    <NavSocials socials={SOCIALS}></NavSocials>
+                    <button
+                        className={`${style["open"]} ${style["button"]} `}
+                        onClick={handleOpen}
+                    >
+                        {MENU_BURGER}
+                    </button>
+                </div>
             )}
         </div>
     );
